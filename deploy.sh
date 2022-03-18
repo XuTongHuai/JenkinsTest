1589797eb0acd4ba4c73f9de178296a9f1547016
许通淮 <360992247@qq.com>
2022-03-18 15:15:57 +0800

    init

--- deploy.sh
+++ deploy.sh
@@ -20,7 +20,7 @@ killTomcat()
       kill -9 $pid
     fi
 }
-cd $PROJ_PATH/order
+cd $PROJ_PATH/JenkinsTest
 mvn clean install
 
 # 停tomcat
@@ -29,13 +29,13 @@ killTomcat
 # 删除原有工程
 rm -rf $TOMCAT_APP_PATH/webapps/ROOT
 rm -f $TOMCAT_APP_PATH/webapps/ROOT.war
-rm -f $TOMCAT_APP_PATH/webapps/order.war
+rm -f $TOMCAT_APP_PATH/webapps/JenkinsTest.war
 
 # 复制新的工程
-cp $PROJ_PATH/order/target/order.war $TOMCAT_APP_PATH/webapps/
+cp $PROJ_PATH/JenkinsTest/target/JenkinsTest.war $TOMCAT_APP_PATH/webapps/
 
 cd $TOMCAT_APP_PATH/webapps/
-mv order.war ROOT.war
+mv JenkinsTest.war ROOT.war
 
 # 启动Tomcat
 cd $TOMCAT_APP_PATH/
